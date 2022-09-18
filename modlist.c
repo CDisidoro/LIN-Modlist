0b837944bb1eb9a976b4daacba46b1f39bed7736
David Perez <echoalizr@gmail.com>
2022-09-18 14:42:48 +0200

    Add files via upload

--- modlist.c
+++ modlist.c
@@ -4,6 +4,7 @@
 #include <linux/vmalloc.h>
 #include <linux/uaccess.h>
 #include <linux/ftrace.h>
+#include <linux/slab.h>
 #include "list.h"
 
 #define BUFFER_LENGTH PAGE_SIZE
@@ -12,64 +13,110 @@ MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Gestion de lista enlazada de enteros");
 MODULE_AUTHOR("Camilo Andres Disidoro - David Manuel Perez Mora");
 
-//TEMPORAL
-static char *command;  //Espacio reservado para el comando
+// TEMPORAL
+static char *command;  // Espacio reservado para el comando
 
-//Declaracion de la estructura base
-struct list_head mylist; //Cabecera de la lista enlazada
-struct list_item { //Estructura representativa de los nodos de la lista
+// Declaracion de la estructura base
+struct list_head mylist;  // Cabecera de la lista enlazada
+int nr_items;
+struct list_item {  // Estructura representativa de los nodos de la lista
     int data;
     struct list_head links;
 };
 
-//Funciones de lectura y escritura del modulo TODO
+// Funciones de lectura y escritura del modulo TODO
 
 static ssize_t modlist_read(struct file *filp, char __user *buf, size_t len, loff_t *off) {
   
-  int nr_bytes;
-  
-  if ((*off) > 0) // No hay nada mas pendiente de leer
-      return 0;
+    int nr_bytes;
+    char *out;
     
-  nr_bytes=strlen(command);
+    if ((*off) > 0)  // No hay nada mas pendiente de leer
+        return 0;
+      
+    nr_bytes = nr_items * (sizeof(int) + 1);  // +1 por el '\n'
+    out = (char *)kmalloc(nr_bytes + 1, GFP_KERNEL);
+      
+    if (len < nr_bytes)
+        return -ENOSPC;
     
-  if (len<nr_bytes)
-    return -ENOSPC;
-  
+    struct list_item* item = NULL;
+    struct list_head* cur_node = NULL;
+    int i = 0;
+
+    list_for_each(cur_node, &mylist) {
+        item = list_entry(cur_node, struct list_item, links);
+        // replace with strcat?
+        out[i] = item -> data;
+        i += sizeof(int);
+        out[i++] = '\n';
+    }
+
     // Transfiere informacion del kernel al espacio de usuario
-  if (copy_to_user(buf, command,nr_bytes))
-    return -EINVAL;
+    if (copy_to_user(buf, out, nr_bytes)) {
+        kfree(out);
+        return -EINVAL;
+    }
     
-  (*off)+=len;  // Actualiza el puntero de fichero
-  return nr_bytes; 
+    kfree(out);
+    (*off) += len;  // Actualiza el puntero de fichero
+    return nr_bytes; 
 }
 
 static ssize_t modlist_write(struct file *filp, const char __user *buf, size_t len, loff_t *off) {
-  int available_space = BUFFER_LENGTH-1;
-  char action[7];
-  int val;
-  if ((*off) > 0) // La aplicacion puede escribir en esta entrada solo una vez
-    return 0;
-  
-  if (len > available_space) {
-    printk(KERN_INFO "Modlist: not enough space!!\n");
-    return -ENOSPC;
-  }
-  
-  // Transifere datos desde espacio usuario a espacio kernel
-  if (copy_from_user( &command[0], buf, len ))  
-    return -EFAULT;
-  sscanf(command, "%s %d", action, &val); //Recoge los parametros que se le ha enviado al modulo
-  trace_printk("New value of command: %s\n", command);
-  trace_printk("Action tracked: %s\n", action);
-  trace_printk("Value tracked, %d\n", val);
-  command[len] = '\0'; // Agrega el `\0'
-  *off+=len;            // actualiza el puntero de fichero
-  
-  return len;
+    int available_space = BUFFER_LENGTH - 1;
+    char action[7];
+    int val;
+    if ((*off) > 0) {  // La aplicacion puede escribir en esta entrada solo una vez
+        return 0;
+    }
+    if (len > available_space) {
+        printk(KERN_INFO "Modlist: not enough space!!\n");
+        return -ENOSPC;
+    }
+    // Transifere datos desde espacio usuario a espacio kernel
+    if (copy_from_user(&command[0], buf, len)) {
+        return -EFAULT;
+    }
+    sscanf(command, "%s %d", action, &val);  // Recoge los parametros que se le ha enviado al modulo
+    trace_printk("New value of command: %s\n", command);
+    trace_printk("Action tracked: %s\n", action);
+    trace_printk("Value tracked, %d\n", val);
+
+    /*
+    switch (action) {
+        case "add":
+            struct list_item item = (struct list_item*)kmalloc(SOMETHING, GFP_KERNEL);
+            break;
+        case "remove":
+            break;
+        case "cleanup":
+            list_cleanup(&mylist);
+            break;
+        default:
+            break;
+    }
+    */
+    
+    command[len] = '\0';  // Agrega el `\0'
+    *off += len;  // actualiza el puntero de fichero
+    
+    return len;
 }
 
-//Gestion del fichero /proc
+/*
+static void list_cleanup(struct list_head* list) {
+    if (!list_empty(list)) {
+        struct list_head* current = NULL;
+        struct list_head* next = NULL;
+        list_for_each_safe(current, next, list) {
+            kfree(current);
+        }
+    }
+}
+*/
+
+// Gestion del fichero /proc
 static struct proc_dir_entry *proc_entry;
 
 static const struct proc_ops proc_entry_fops = {
@@ -77,20 +124,20 @@ static const struct proc_ops proc_entry_fops = {
     .proc_write = modlist_write,    
 };
 
-//Carga y Descarga del modulo
-int init_modlist_module(void){
-    int ret=0;
+// Carga y descarga del modulo
+int init_modlist_module(void) {
+    int ret = 0;
     INIT_LIST_HEAD(&mylist);
-    command = (char *)vmalloc( BUFFER_LENGTH );//TEMP
-    if(!command){
+    command = (char *)vmalloc(BUFFER_LENGTH);  // TEMP
+    if (!command) {
         return -ENOMEM;
-    }else{
+    } else {
         proc_entry = proc_create( "modlist", 0666, NULL, &proc_entry_fops);
         if (proc_entry == NULL) {
-        ret = -ENOMEM;
-        printk(KERN_INFO "Modlist: Can't create /proc entry\n");
+            ret = -ENOMEM;
+            printk(KERN_INFO "Modlist: Can't create /proc entry\n");
         } else {
-        printk(KERN_INFO "Modlist: Module loaded\n");
+            printk(KERN_INFO "Modlist: Module loaded\n");
         }
     }
     return ret;
@@ -98,11 +145,18 @@ int init_modlist_module(void){
 
 void exit_modlist_module(void){
     remove_proc_entry("modlist", NULL);
-    vfree(command);//TEMP
-    
+    vfree(command);  // TEMP
+    // list_cleanup(&mylist);
+    if (!list_empty(&mylist)) {
+        struct list_head* cur_node = NULL;
+        struct list_head* next = NULL;
+        list_for_each_safe(cur_node, next, &mylist) {
+            kfree(cur_node);
+        }
+    }
     printk(KERN_INFO "Modlist: Module unloaded.\n");
 }
 
-//Enlazado de las funciones de carga/descarga
+// Enlazado de las funciones de carga/descarga
 module_init(init_modlist_module);
 module_exit(exit_modlist_module);
